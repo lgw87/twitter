0cbb5e95d69109436973215eff037f7ef412f183
Lee Gwang Won <lgw@Leeui-MacBookAir.local>
2022-08-19 23:20:02 +0900

    Updates

---
+++ precache-manifest.bcacb613b4f3ef7db882e90930119b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3829292e8d35879ac0c",
+    "url": "/twitter/static/css/main.b7256a97.chunk.css"
+  },
+  {
+    "revision": "b3829292e8d35879ac0c",
+    "url": "/twitter/static/js/main.b3829292.chunk.js"
+  },
+  {
+    "revision": "d5cbe84a71484ddc7c93",
+    "url": "/twitter/static/js/1.d5cbe84a.chunk.js"
+  },
+  {
+    "revision": "3f89907d52536c66f970",
+    "url": "/twitter/static/js/runtime~main.3f89907d.js"
+  },
+  {
+    "revision": "61a4919459d046f1b71aa0708768ce4f",
+    "url": "/twitter/index.html"
+  }
+];
